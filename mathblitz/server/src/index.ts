c0033327f6e27eebed5ba9590bc466cde8f53d7e
Aidan Chang <aidan.chang@outlook.com>
2026-02-27 01:32:53 -0600

    create basic backend websocket example

---
+++ mathblitz/server/src/index.ts
@@ -0,0 +1,11 @@
+import { WebSocketServer } from "ws";
+
+const server = new WebSocketServer({ port: 8080 });
+
+server.on('connection', socket => {
+
+    socket.on('message', message => {
+
+        socket.send(`Echo: ${message}`);
+    })
+})
